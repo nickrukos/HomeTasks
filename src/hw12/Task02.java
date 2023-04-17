aea965bd82dc841450a35210ae15371ce3e00492
Nicholas Rukosuev <nickrukos@mail.ru>
2023-04-18 00:01:45 +0300

    Домашнее задание №12

---
+++ src/hw12/Task02.java
@@ -0,0 +1,73 @@
+package hw12;
+
+import java.util.*;
+
+public class Task02 {
+    public static List <String> firstTask(HashMap<String, String> map, String city)
+    {
+        ArrayList<String> list = new ArrayList<>();
+        for (Map.Entry<String, String> entry : map.entrySet())
+        {
+            if(entry.getValue().equals(city))
+            {
+                list.add(entry.getKey());
+            }
+        }
+        return list;
+    }
+
+    public static HashMap<String,Integer > wordsCount(List<String> list)
+    {
+        HashMap<String,Integer>
+    }
+
+    public static void main(String[] args) {
+        // FIXME:: В решениях не использовать lambda выражения и stream API
+
+        // TODO (2.1) написать статический метод, который принимает на вход map (например, firstTaskMap) и город (например, city),
+        //  формирует и возвращает список (List) логинов, которые соответствуют переданному городу
+
+        HashMap<String, String> firstTaskMap = new HashMap<>();
+        firstTaskMap.put("qwe", "Тверь");
+        firstTaskMap.put("asd", "Тверь");
+        firstTaskMap.put("zxc", "Москва");
+        firstTaskMap.put("rty", "Тверь");
+        firstTaskMap.put("fgh", "Магадан");
+
+        String city = "Тверь";
+
+
+        // TODO (2.2) дан список слов (например, words).
+        //  Написать статический метод, который будет возвращать количество одинаковых слов в списке
+        //  в map вида Map<String, Integer>, где String - слово и Integer - количество повторений
+
+        List<String> words = new ArrayList<>();
+        words.add("may");
+        words.add("august");
+        words.add("june");
+        words.add("may");
+        words.add("may");
+        words.add("july");
+        words.add("may");
+        words.add("august");
+        words.add("august");
+
+
+        // TODO (2.3) Задания по тексту (например, text). На каждый пункт - минимум один метод (можно статический):
+        //  1. написать метод, принимающий на вход слово и возвращающий частоту встречаемости данного слова в тексте
+        //  (Map<String, Long>, где - ключи (String) - слово, значения (Long) - частота встречаемости)
+        //  2. написать метод, который собирает все слова в группы по количеству букв:
+        //  например, в одну группу попадут слова: 3 - [the, war, jar, get, met...], в другую 2 - [on, up, no, of...]
+        //  (Map<Integer, ArrayList<String>>, где - ключи (Integer) - количество букв, значения (ArrayList<String>) - слова)
+        //  3. написать метод, который выводит в консоль топ 10 самых часто встречаемых в тексте слов (артикли и предлоги тоже считаем за слова)
+
+        // в тексте содержатся только буквы и пробельные символы
+        String text = "It is a uncover long established fact that a reader will be distracted uncover by the readable content of a page " +
+                "when looking at its layout The point of using uncover Lorem Ipsum is that sites it has a more-or-less normal distribution of letters" +
+                "as uncover opposed to still using Content here humour uncover content here making it look like readable English Many desktop publishing " +
+                "packages and web page editors now use Lorem Ipsum as their default model text and a search for lorem ipsum will " +
+                "uncover many web sites still uncover in their infancy Various versions uncover have evolved over the years uncover sometimes by accident" +
+                " sometimes on purpose injected humour and the like";
+
+    }
+}
